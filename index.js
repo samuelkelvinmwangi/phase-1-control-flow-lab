6ec785b73bc30864bebfc74be75a2c6db3a6ed63
samuel <samuelkelvinmwangi@gmail.com>
2023-03-28 21:36:43 +0300

    assingnment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+      return `This one is on me!`
+  }
+  else if (feet >= 2000) {
+    return `I will gladly take your thirty bucks.`
+  }
+  else if (feet > 2500) {
+    return `No can do.`
+  }
+  else {
+    return `This one is on me!`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return `Thank you so much.`;
+
+    case 'not as generous':
+      return `Thank you.`
+
+    default:
+      return `Bye.`;
+  }
+   
 }
